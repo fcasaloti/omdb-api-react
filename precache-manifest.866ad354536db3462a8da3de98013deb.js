981503e7890d64ff471ce2dc6264ed367109f6de
fcasaloti <fcasaloti@hotmail.com>
2020-09-07 11:03:31 -0700

    Updates

---
+++ precache-manifest.866ad354536db3462a8da3de98013deb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2cbd199c5382ff88040dc400f92e48a",
+    "url": "/omdb-api-react/index.html"
+  },
+  {
+    "revision": "47ec7f0155ccb681b3a7",
+    "url": "/omdb-api-react/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5f1fc2d7b7a833d38af4",
+    "url": "/omdb-api-react/static/css/main.ccb18b81.chunk.css"
+  },
+  {
+    "revision": "47ec7f0155ccb681b3a7",
+    "url": "/omdb-api-react/static/js/2.3efc0c61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/omdb-api-react/static/js/2.3efc0c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1fc2d7b7a833d38af4",
+    "url": "/omdb-api-react/static/js/main.3505058c.chunk.js"
+  },
+  {
+    "revision": "46dc1c43192ce922f408",
+    "url": "/omdb-api-react/static/js/runtime-main.800d237f.js"
+  }
+]);
